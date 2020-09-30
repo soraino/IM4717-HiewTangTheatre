3d744e8cfc86d20a6ad1e193381395dcdc2c38ce
Hiew Jin Nee <hiewjinneeeee@gmail.com>
2020-09-30 16:35:48 +0800

    Movie details page

---
+++ movieView.js
@@ -0,0 +1,17 @@
+// var elements = document.getElementsByClassName("column");
+
+// var i;
+
+// // List View
+// function listView() {
+//     for (i = 0; i < elements.length; i++) {
+//         elements[i].style.width = "100%";
+//     }
+// }
+
+// // Grid View
+// function gridView() {
+//     for (i = 0; i < elements.length; i++) {
+//         elements[i].style.width = "180px";
+//     }
+// }
