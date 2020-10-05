c06478c282f8453a6f6c86e94f3c18189095ac23
soraino <personalstuff97@gmail.com>
2020-10-05 21:56:27 +0800

    refactor code and file structure

--- moviesView.php
+++ moviesView.php
@@ -5,10 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>HiewTang Theatre</title>
-    <link rel="stylesheet" href="./main.css" />
-    <link rel="stylesheet" href="./index.css" />
-    <link rel="stylesheet" href="./moviesView.css" />
-    <!-- <script src="movieView.js"></script> -->
+    <link rel="stylesheet" href="./css/main.css" />
+    <link rel="stylesheet" href="./css/moviesView.css" />
 </head>
 
 <body>
--- seatings.php
+++ seatings.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./main.css" />
-    <link rel="stylesheet" href="./index.css" />
-    <link rel="stylesheet" href="./seatings.css" />
+    <link rel="stylesheet" href="./css/main.css" />
+    <link rel="stylesheet" href="./css/seatings.css" />
     <title>Seats selection</title>
 </head>
 
