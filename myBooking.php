13fc59c208dcf40d0862350f052eec80de6ef9be
soraino <personalstuff97@gmail.com>
2020-10-25 00:12:41 +0800

    my Booking now works

--- myBooking.php
+++ myBooking.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>HiewTang Theatre</title>
     <link rel="stylesheet" href="./css/main.css" />
+    <link rel="stylesheet" href="./css/myBooking.css" />
     <link rel="icon" type="image/png" href="assets/logo/favicon.ico" />
 </head>
 
@@ -16,34 +17,35 @@
         $userData = $_SESSION["user".$userId];
         include "./navbar.php";
         @$DB = new mysqli('localhost', 'f34ee', 'f34ee', 'f34ee');
-        $BOOKING_QUERY = "select B.PremiereDate, TIME_FORMAT(T.StartTime, '%h:%i %p') as StartTime, TH.Location, TH.Number, M.Name from f34ee.Booking as B inner join f34ee.Timeslot as T on T.id = B.TimeslotId inner join f34ee.Theatre as TH on TH.id = T.TheatreId inner join f34ee.MovieDetail as M on M.Id = T.MovieDetailId where B.Id = '".$_GET['bId']."' ;";
-        $SEAT_NUMBER_QUERY = "select CONCAT(S.`Row`,'',S.`Column`) as seatNumber from f34ee.Seating as S inner join f34ee.Ticket as T on T.SeatId = S.Id where T.BookingId = '".$_GET['bId']."' ;";
-        if(mysqli_connect_errno()){
-            echo 'Error: Could not connect to database.';
-            exit;
-        }
-        $bookingReults = $DB->query($BOOKING_QUERY);
-        $seatNumberReults = $DB->query($SEAT_NUMBER_QUERY);
-        $date;
-        $startTime;
-        $location;
-        $theatreNumber;
-        $movieName;
-        $seatNumbers = array();
-        if($bookingReults->num_rows > 0){
-            $row = $bookingReults->fetch_assoc();
-            $date=date_create($row['PremiereDate']);
-            $startTime = $row['StartTime'];
-            $location = $row['Location'];
-            $theatreNumber = $row['Number'];
-            $movieName = $row['Name'];
-        }
-        if($seatNumberReults->num_rows > 0){
-            while($row = $seatNumberReults -> fetch_assoc()){
-                array_push($seatNumbers, $row['seatNumber']);
+        if(isset($_GET['bId'])){
+            $BOOKING_QUERY = "select B.PremiereDate, TIME_FORMAT(T.StartTime, '%h:%i %p') as StartTime, TH.Location, TH.Number, M.Name from f34ee.Booking as B inner join f34ee.Timeslot as T on T.id = B.TimeslotId inner join f34ee.Theatre as TH on TH.id = T.TheatreId inner join f34ee.MovieDetail as M on M.Id = T.MovieDetailId where B.Id = '".$_GET['bId']."' ;";
+            $SEAT_NUMBER_QUERY = "select CONCAT(S.`Row`,'',S.`Column`) as seatNumber from f34ee.Seating as S inner join f34ee.Ticket as T on T.SeatId = S.Id where T.BookingId = '".$_GET['bId']."' ;";
+            if(mysqli_connect_errno()){
+                echo 'Error: Could not connect to database.';
+                exit;
             }
-        }
-    ?>
+            $bookingReults = $DB->query($BOOKING_QUERY);
+            $seatNumberReults = $DB->query($SEAT_NUMBER_QUERY);
+            $date;
+            $startTime;
+            $location;
+            $theatreNumber;
+            $movieName;
+            $seatNumbers = array();
+            if($bookingReults->num_rows > 0){
+                $row = $bookingReults->fetch_assoc();
+                $date=date_create($row['PremiereDate']);
+                $startTime = $row['StartTime'];
+                $location = $row['Location'];
+                $theatreNumber = $row['Number'];
+                $movieName = $row['Name'];
+            }
+            if($seatNumberReults->num_rows > 0){
+                while($row = $seatNumberReults -> fetch_assoc()){
+                    array_push($seatNumbers, $row['seatNumber']);
+                }
+            }
+        ?>
     <main class="container center-text" style="margin-top: 150px">
         <h2>YOUR BOOKING DETAILS</h2>
         <h2>
@@ -54,13 +56,13 @@
             Reference ID: <?php echo $_GET['bId']; ?><br />
             Cinema: <?php echo $location; ?>, Theatre <?php echo $theatreNumber; ?> <br />
             Seat: <?php 
-            for($i = 0 ; $i< count($seatNumbers); $i++){
-                echo strtoupper($seatNumbers[$i]);
-                if(count($seatNumbers) > 1 && $i < count($seatNumbers) -1 ){
-                    echo ', ';
+                for($i = 0 ; $i< count($seatNumbers); $i++){
+                    echo strtoupper($seatNumbers[$i]);
+                    if(count($seatNumbers) > 1 && $i < count($seatNumbers) -1 ){
+                        echo ', ';
+                    }
                 }
-            }
-            ?><br />
+                ?><br />
             Date: <?php echo date_format($date,"d/m/Y"); ?><br />
             Time: <?php echo $startTime; ?><br />
         </h3>
@@ -73,6 +75,66 @@
         <br />
         <a href="./" class="button">Return to Home Page</a>
     </main>
+    <?php 
+    $bookingReults->free();
+    $seatNumberReults->free();
+    $DB->close();
+    }
+    else{
+    ?>
+    <main class="container">
+        <h1>All Bookings</h1>
+        <?php
+        $BOOKING_QUERY = "select B.Id, B.PremiereDate, TIME_FORMAT(T.StartTime, '%h:%i %p') as StartTime, TH.Location, TH.Number, M.Name from f34ee.Booking as B inner join f34ee.Timeslot as T on T.id = B.TimeslotId inner join f34ee.Theatre as TH on TH.id = T.TheatreId inner join f34ee.MovieDetail as M on M.Id = T.MovieDetailId where B.UserId = '".$userId."' ;";
+        $bookingResults = $DB->query($BOOKING_QUERY);
+        if($bookingResults->num_rows>0){
+            while($row = $bookingResults->fetch_assoc()){
+                $SEAT_NUMBER_QUERY = "select CONCAT(S.`Row`,'',S.`Column`) as seatNumber from f34ee.Seating as S inner join f34ee.Ticket as T on T.SeatId = S.Id where T.BookingId = '".$row['Id']."' ;";
+                $seatNumberReults = $DB->query($SEAT_NUMBER_QUERY);
+                $date=date_create($row['PremiereDate']);
+                $startTime = $row['StartTime'];
+                $location = $row['Location'];
+                $theatreNumber = $row['Number'];
+                $movieName = $row['Name'];
+                $BookingId = $row['Id'];
+                $seatNumbers = array();
+                if($seatNumberReults->num_rows > 0){
+                    while($row = $seatNumberReults -> fetch_assoc()){
+                        array_push($seatNumbers, $row['seatNumber']);
+                    }
+                }
+                ?>
+        <div class="row box">
+            <div class="col size-6" style="text-align: center;">
+                <h4>
+                    Reference ID:<?php echo $BookingId; ?><br />
+                    Cinema: <?php echo $location; ?>, Theatre <?php echo $theatreNumber; ?><br />
+                    Seat:<?php 
+                for($i = 0 ; $i< count($seatNumbers); $i++){
+                    echo strtoupper($seatNumbers[$i]);
+                    if(count($seatNumbers) > 1 && $i < count($seatNumbers) -1 ){
+                        echo ', ';
+                    }
+                }
+                ?>
+                </h4>
+            </div>
+            <div class="col size-6" style="text-align: center;">
+                <h4>
+                Movie Name: <?php echo $movieName;?><br>
+                Date: <?php echo date_format($date,"d/m/Y"); ?><br>
+                Time: <?php echo $startTime; ?>
+                </h4>
+            </div>
+        </div>
+        <?php
+            }
+        }
+        $DB->close();
+    ?>
+    </main>
+    <?php
+    } ?>
     <footer class="footer">
         <div class="footer-content">
             <div class="container">
--- register.php
+++ register.php
@@ -13,44 +13,44 @@ if (
 if ($_POST['password'] != $_POST['confirmPassword']) {
     $DB->close();
 ?>
-    <script>
-        alert("Password and Confirm Password are not the same.")
-        window.location.replace('./login.html')
-    </script>
+<script>
+alert("Password and Confirm Password are not the same.")
+window.location.replace('./register.html')
+</script>
 <?php
 }
 $DbValidation = true;
-$QUERY_EMAIL = "SELECT Email FROM f34ee.User WHERE Email = '" . trim($_POST['email']) . "';";
-$QUERY_PHONENUMBER = "SELECT PhoneNumber FROM f34ee.User WHERE Email = '" . trim($_POST['phone']) . "';";
-$QUERY_CARDNUMBER = "SELECT CardNumber FROM f34ee.Card WHERE CardNumber = '" . trim($_POST['creditCardNumber']) . "';";
+$QUERY_EMAIL = "SELECT Email FROM f34ee.User WHERE Email = '".$_POST['email']."';";
+$QUERY_PHONENUMBER = "SELECT PhoneNumber FROM f34ee.User WHERE Email = '".$_POST['phone']."';";
+$QUERY_CARDNUMBER = "SELECT CardNumber FROM f34ee.Card WHERE CardNumber = '".$_POST['creditCardNumber']."';";
 
 $emailResults = $DB->query($QUERY_EMAIL);
 $phoneNumberResults = $DB->query($QUERY_PHONENUMBER);
 $cardNumberResults = $DB->query($QUERY_CARDNUMBER);
 
 // Validation
-if ($emailResults->num_row > 0) {
+if ($emailResults->num_rows > 0) {
     $DbValidation = false;
 ?>
-    <script>
-        alert('Email already exist.');
-    </script>
+<script>
+alert('Email already exist.');
+</script>
 <?php
 }
-if ($phoneNumberResults->num_row > 0) {
+if ($phoneNumberResults->num_rows > 0) {
     $DbValidation = false;
 ?>
-    <script>
-        alert('Phone number has been use.');
-    </script>
+<script>
+alert('Phone number has been use.');
+</script>
 <?php
 }
-if ($cardNumberResults->num_row > 0) {
+if ($cardNumberResults->num_rows > 0) {
     $DbValidation = false;
 ?>
-    <script>
-        alert('Card Number is already in use.');
-    </script>
+<script>
+alert('Card Number is already in use.');
+</script>
 <?php
 }
 
@@ -62,11 +62,14 @@ $cardNumberResults->free();
 if (!$DbValidation) {
     $DB->close();
 ?>
-    <script>
-        // window.location.replace('./register.html');
-    </script>
+<script>
+window.history.back();
+</script>
 <?php
 }
+$emailResults->free();
+$phoneNumberResults->free();
+$cardNumberResults->free();
 // end of validation
 
 $INSERT_USER_QUERY = "INSERT INTO f34ee.User( Name, Email, PhoneNumber, Password) VALUES ('" . trim($_POST['name']) . "','" . trim(urldecode($_POST['email'])) . "','" . $_POST['phone'] . "','" . md5(utf8_decode(urldecode($_POST['password']))) . "');";
@@ -90,11 +93,18 @@ if ($userInsertResult) {
                 $userData = array(
                     'Id' => $id,
                     'PhoneNumber' => $_POST['phone'],
-                    'Name' => $_POST['Nnameame'],
+                    'Name' => $_POST['name'],
+                    'Email' => $_POST['email'],
                 );
                 session_start();
                 setcookie($name = 'userId', $value = $id, $expire = time() + (3600 * 24 * 7), $path = "", $domain = "", $secure = false, $httponly = false);
                 $_SESSION[$id] = $userData;
+                $to = 'f34ee@localhost';
+                $subject = 'Welcome to HiewTang Theatres';
+                $message = "Hi ".$_POST['name'].",\nWelcome to HiewTang Theatres. You can now book movie tickets with us!\n\nHappy Movie Viewing!";
+                $headers = 'From: message@hiewtangtheatres.com' . "\r\n".'Reply-To: f34ee@localhost' . "\r\n".'X-Mailer: PHP/'. phpversion();
+                mail($to,$subject,$message,$headers,'-f34ee@localhost');
+
                 header('location: index.php');
             } else {
                 echo 'Unexpected error has occured <a href="./">Return to home</a>';
